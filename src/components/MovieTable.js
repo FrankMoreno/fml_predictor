2b60818d3bf056c4c238a2422ea74f71f479df5b
Frank Moreno <Frank.Moreno95@gmail.com>
2019-02-12 12:59:26 -0600

    Added eslint and movie posters by each title

--- src/components/MovieTable.js
+++ src/components/MovieTable.js
@@ -11,25 +11,27 @@ class MovieTable extends Component {
         this.solveForScreens = this.solveForScreens.bind(this);
         this.getEstimates = this.getEstimates.bind(this);
     }
-    
+
     componentDidMount() {
         // TODO Add error handling 
-        // TODO Check what's making this so slow
+        // TODO Cache this somewhere
         fetch('https://thanx-fml-api.herokuapp.com/movies')
             .then(res => res.json())
             .then(result => {
                 let newMovies = {}
                 result.movies.forEach(movie => {
                     newMovies[movie.name] = {
-                        "bux" : (movie.bux).replace(/\$/g,''),
-                        "estimate" : "",
-                        "screens":"1"
+                        "bux": (movie.bux).replace(/\$/g, ''),
+                        "posterLink": movie.posterLink,
+                        "estimate": "",
+                        "screens": "1"
                     }
-                })
+                });
+
                 this.setState({
-                    movies : newMovies,
-                    isLoaded : true,
-                    results : {}
+                    movies: newMovies,
+                    isLoaded: true,
+                    results: {}
                 });
             })
     }
@@ -37,21 +39,24 @@ class MovieTable extends Component {
     onEstimateChange(movieName, event) {
         let newMovies = this.state.movies;
         newMovies[movieName].estimate = event.target.value;
-        this.setState({movies : newMovies})
+        this.setState({ movies: newMovies })
     }
-    
+
     makeListItems() {
         // TODO Figure out how to make listItem into it's own component 
         let listItems = []
-        for(let movieName in this.state.movies) {
+        for (let movieName in this.state.movies) {
             // TODO Figure out how to not go from array to object to array
             listItems.push(
                 <tr key={movieName}>
+                    <td>
+                        <img className='moviePosterImage' src={this.state.movies[movieName].posterLink} alt='Movie Poster' />
+                    </td>
                     <td>{movieName}</td>
                     <td>{this.state.movies[movieName].bux}</td>
                     <td>
-                        <textarea 
-                            value={this.state.movies[movieName].estimate} 
+                        <textarea
+                            value={this.state.movies[movieName].estimate}
                             onChange={(event) => this.onEstimateChange(movieName, event)}
                         />
                     </td>
@@ -62,39 +67,40 @@ class MovieTable extends Component {
     }
 
     solveForScreens() {
-        // TODO Replace this library with something better supported
+        // TODO Look into other Linear Programming libraries
         let solver = require("../../node_modules/javascript-lp-solver/src/solver"),
-        updatedResults,
-        model = {
-            "optimize" : "estimate",
-            "opType" : "max",
-            "constraints" : {
-                "bux" : {"max":"1000"},
-                "screens" : {"max" : "8"}
-            },
-            "variables" : this.state.movies
-        };
+            updatedResults,
+            model = {
+                "optimize": "estimate",
+                "opType": "max",
+                "constraints": {
+                    "bux": { "max": "1000" },
+                    "screens": { "max": "8" },
+                },
+                "variables": this.state.movies
+            };
         updatedResults = solver.Solve(model);
-        this.setState({results:updatedResults});
+        this.setState({ results: updatedResults });
     }
 
+    // TODO Error handling
     getEstimates() {
         fetch('https://thanx-fml-api.herokuapp.com/estimates')
-        .then(res => res.json())
-        .then(result => {
-            result.estimates.forEach(estimate => {
-                let newMovies = this.state.movies;
-                if(this.state.movies.hasOwnProperty(estimate.name)) {
-                    let newEstimate = estimate.estimate.replace(/\$/g,'')
-                    newMovies[estimate.name].estimate = parseFloat(newEstimate);
-                }
-                this.setState({movies : newMovies});
-            })
-        })
+            .then(res => res.json())
+            .then(result => {
+                result.estimates.forEach(estimate => {
+                    let newMovies = this.state.movies;
+                    if (this.state.movies.hasOwnProperty(estimate.name)) {
+                        let newEstimate = estimate.estimate.replace(/\$/g, '')
+                        newMovies[estimate.name].estimate = parseFloat(newEstimate);
+                    }
+                    this.setState({ movies: newMovies });
+                });
+            });
     }
 
     render() {
-        if(this.state.isLoaded === false) {
+        if (this.state.isLoaded === false) {
             return <h1>Loading...</h1>
         }
         else {
@@ -104,9 +110,10 @@ class MovieTable extends Component {
                     <table>
                         <thead>
                             <tr>
-                                <td>Title</td>
-                                <td>Bux</td>
-                                <td>Estimate</td>
+                                <th></th>
+                                <th>Title</th>
+                                <th>Bux</th>
+                                <th>Estimate</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -115,7 +122,7 @@ class MovieTable extends Component {
                     </table>
                     <button onClick={this.solveForScreens}>Submit</button>
                     <button onClick={this.getEstimates}>Get Estimates</button>
-                    <MovieResults results={this.state.results}/>
+                    <MovieResults results={this.state.results} />
                 </div>
             );
         }

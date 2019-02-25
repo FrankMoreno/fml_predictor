4a993c7eddd15a2ce27d7c72798b238df2223a9f
Frank Moreno <Frank.Moreno95@gmail.com>
2019-02-24 23:02:36 -0600

    Move function around bc honestly idk how to split this component anymore

--- src/components/MovieResultsTable.js
+++ src/components/MovieResultsTable.js
@@ -2,30 +2,30 @@ import React, { Component } from 'react'
 import MovieRow from './MovieRow';
 
 class MovieResultsTable extends Component {
+    MovieRows() {
+        let results = []
+            for(let key in this.props.results) {
+                if(key !== 'feasible' && key !== 'bounded' && key !== 'result') {
+                    results.push(
+                        <MovieRow key={key} name={key} total={this.props.results[key]}/>
+                    );
+                }
+            }
+            return results;
+    }
+
     render() {
         return (
             <div>
                 <h1>Results</h1>
                 <table>
                     <tbody>
-                        {MovieRows(this.props)}
+                        {this.MovieRows()}
                     </tbody>
                 </table>
             </div>
         );
     }
-}
-
-function MovieRows(props) {
-    let results = []
-        for(let key in props.results) {
-            if(key !== 'feasible' && key !== 'bounded' && key !== 'result') {
-                results.push(
-                    <MovieRow key={key} name={key} total={props.results[key]}/>
-                );
-            }
-        }
-        return results;
 }
 
 export default MovieResultsTable;
